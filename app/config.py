77fe4d21b92521b7cce4ee92aacea18384235939
Chan Kyu Kim <chk.kim87@gmail.com>
2025-09-16 07:50:38 +0900

    added jira_project_key, jira_max_results conf

--- app/config.py
+++ app/config.py
@@ -4,3 +4,5 @@ SESSION_COOKIE_NAME = "session_id"
 SESSION_EXPIRE_SECONDS = int(os.getenv("SESSION_EXPIRE_SECONDS", 3600))
 REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
 JIRA_BASE_URL = os.getenv("JIRA_BASE_URL", "https://de101.atlassian.net/")
+JIRA_PROJECT_KEY = os.getenv("JIRA_PROJECT_KEY", "DATA")
+JIRA_MAX_RESULTS = int(os.getenv("JIRA_MAX_RESULTS", "3"))
