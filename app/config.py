b0e4a8804997435cd9335911e25525a1b9efe4ae
ChanKyu Kim <chk.kim87@gmail.com>
2025-10-23 16:08:18 +0900

    formatted config.py and added Mysql and confs for sample_data_generation and data extraction

--- app/config.py
+++ app/config.py
@@ -1,11 +1,30 @@
 import os
 
+# session
 SESSION_COOKIE_NAME = "session_id"
 SESSION_EXPIRE_SECONDS = int(os.getenv("SESSION_EXPIRE_SECONDS", 3600))
+
+# redis
 REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
+
+# jira
 JIRA_BASE_URL = os.getenv("JIRA_BASE_URL", "https://de101.atlassian.net/")
 JIRA_ADMIN_GROUP = os.getenv("JIRA_BASE_URL", "jira-admins-de101")
 JIRA_PROJECT_KEY = os.getenv("JIRA_PROJECT_KEY", "DATA")
 JIRA_MAX_RESULTS = int(os.getenv("JIRA_MAX_RESULTS", "3"))
+
+# MySQL
+MYSQL_HOST = os.getenv("MYSQL_HOST", "127.0.0.1")
+MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "root")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "data_request")
+ALLOW_LOCAL_INFILE = True
+
+# file related
 FILE_PATH = os.getenv("FILE_PATH", "file_path/")
+SAMPLE_NUM_USERS = 1000000
+CHUNK_SIZE = 100000
+
+# slack
 SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL", "secret")
